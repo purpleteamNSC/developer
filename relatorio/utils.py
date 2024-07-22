6477c9fdd4016807d36bc0cfc0c261245253410c
Purple Team <118991961+purpleteamNSC@users.noreply.github.com>
2024-07-22 14:11:51 +0000

    feat: :sparkles: add feature

---
+++ relatorio/utils.py
@@ -0,0 +1,16 @@
+base_url = 'https://apps.fireeye.com'
+
+months_br = {
+    1: "janeiro",
+    2: "fevereiro",
+    3: "março",
+    4: "abril",
+    5: "maio",
+    6: "junho",
+    7: "julho",
+    8: "agosto",
+    9: "setembro",
+    10: "outubro",
+    11: "novembro",
+    12: "dezembro"
+}
