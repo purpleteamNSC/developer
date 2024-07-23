98142c67776aef93481adce9d5b30fde50f83936
Diogo Caldas <purpleteam.diogovdcpa@gmail.com>
2024-07-23 11:17:45 -0300

    separating test code

--- helix/app.py
+++ helix/app.py
@@ -4,16 +4,19 @@ from Helix import Helix_T
 
 load_dotenv()
 
-org = os.getenv('ORG')
-helix_id = os.getenv('HELIX_ID')
-client_id = os.getenv('CLIENT_ID')
-secret = os.getenv('SECRET')
-scope = os.getenv('SCOPE')
+org = os.getenv('X_ORG')
+helix_id = os.getenv('X_HELIX_ID')
+client_id = os.getenv('X_CLIENT_ID')
+secret = os.getenv('X_SECRET')
+scope = os.getenv('X_SCOPE')
 
-nsc = Helix_T(org, helix_id, client_id, secret, scope)
+x = Helix_T(org, helix_id, client_id, secret, scope)
 
 # token = nsc.get_access_token()
-
-researches = nsc.get_search_saved()
+# print(token)
+# researches = nsc.get_index('class=analytics | groupby analytic')
 # for r in researches.get('results', []):
 #     print(f"{r['name']} - {r['query']}")
+
+# x.get_index('class=analytics | groupby analytic')
+print(x.get_search_saved())
